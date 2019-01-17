3ec1fd3b5c23f79627e936c0a708cc621f8fe9e5
mrchnk <mrchnk@gmail.com>
2019-01-18 01:29:07 +0300

    fix whuitespace

--- src/Main.hx
+++ src/Main.hx
@@ -1,22 +1,12 @@
-
-
 import openfl.display.Sprite;
 import openfl.Assets;
-import openfl.text.TextField;
-import openfl.text.TextFormat;
-import openfl.geom.Matrix;
 
 class Main extends Sprite {
 
-
-	public function new () {
-
-		super ();
-
-		var sprite: Sprite = Assets.getMovieClip("library:resources.BankNewWindowSource");
-
-		addChild(sprite);
-
-	}
+  public function new () {
+    super ();
+    var sprite: Sprite = Assets.getMovieClip("library:resources.BankNewWindowSource");
+    addChild(sprite);
+  }
 
 }
